5d05e873d26764e8d16a51c7c9effd873f935d54
ranadeeb92 <62123706+ranadeeb92@users.noreply.github.com>
2021-11-22 12:30:29 -0800

    updated routes
    Co-authored-by: SamClark <63471713+clarksam19@users.noreply.github.com>
    Co-authored-by: ReginaDonovan <55225918+rgdonovan@users.noreply.github.com>
    Co-authored-by: JustinLo <50888159+justinnnlo@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -1,16 +1,34 @@
 const express = require("express");
+const s3 = require("./s3Client");
+const es = require("./esClient");
+const { filterBasedOnDate } = require("./helpers/filterBasedOnDate");
 const app = express();
 const PORT = 3000;
 
 let options = {
   bucketName: "bandstand-s3",
-  prefix = "folder1"
+  prefix: "folder1",
 };
 
-app.get("/s3", (req, res) => {
+app.get("/s3", async (req, res) => {
   let startDate = { year: 2021, month: 10, day: 22 };
   let endDate = { year: 2021, month: 10, day: 23 };
-  res.send("Welcome to s3");
+
+  let objectsList = await s3.listObjectsKeys(
+    options.bucketName,
+    options.prefix
+  );
+  let filteredObjectKeys = filterBasedOnDate(objectsList, startDate, endDate);
+
+  for (let index = 0; index < filteredObjectKeys.length; index++) {
+    let object = await s3.getObject(
+      options.bucketName,
+      filteredObjectKeys[index]
+    );
+    es.pushToES(object);
+  }
+
+  // res.send(filteredObjectKeys);
 });
 
 app.listen(PORT, () => {
